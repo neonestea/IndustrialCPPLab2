0d6b4445bdaa0d56830f94d3d7cd348038eec4ed
neonestea <44156406+neonestea@users.noreply.github.com>
2023-05-10 11:28:19 +0300

    Update shuffler.hpp

--- HW22/HW22/MapReduce/include/mapreduce/shuffler.hpp
+++ HW22/HW22/MapReduce/include/mapreduce/shuffler.hpp
@@ -16,15 +16,9 @@ public:
         //std::cout  << "Shuffler Run thread id " << id  << std::endl;
         for (int i = id; i < store.get_amount(); i += batch_size)
         {
-            //std::cout  << " Thread id " << id << " i = " << i << std::endl;
             auto res = store.get_ith(i);
             if (res) {
                 auto taken = std::move(*res);
-                
-                for (auto val : taken.second)
-                {
-                    std::cout  << " Thread id " << id << " i = " << i << " key = " << taken.first << " value = " << val << std::endl;
-                }
 
                 shuffled.pushback(taken.first , taken.second );
             
