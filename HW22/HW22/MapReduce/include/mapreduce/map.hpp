598c629afc5603fa5d3c5e88431b4e4bb37f4a7c
neonestea <44156406+neonestea@users.noreply.github.com>
2023-05-15 20:23:49 +0300

    Update map.hpp

--- HW22/HW22/MapReduce/include/mapreduce/map.hpp
+++ HW22/HW22/MapReduce/include/mapreduce/map.hpp
@@ -7,16 +7,18 @@
 
 class Map {
 public:
-    void map(std::string filename, Storage& store) {
-        std::ifstream ifs(filename);
-        //std::cout << "Key " << filename << std::endl;
-                                
-        for (const auto& word : boost::range::istream_range<std::string>(ifs))
-        {
-            //std::cout << "word " << word << std::endl;
-            if (word.size() > 100)
-                continue;
-            store.pushback(word, 1);
+    void map(/*std::string filename,*/ Storage& store, int id, std::vector<std::string>& filenames, int batch_size, int total) {
+        for (int i = id; i < total; i += batch_size ) {
+            std::ifstream ifs(filenames[i]);
+            //std::cout << "Key " << filename << std::endl;
+                                    
+            for (const auto& word : boost::range::istream_range<std::string>(ifs))
+            {
+                //std::cout << "word " << word << std::endl;
+                if (word.size() > 100)
+                    continue;
+                store.pushback(word, 1);
+            }
         }
     }
 };
